ddf52fb0f58328a59f19715e6acb50a62bd161c0
JensenM <2502308799@qq.com>
2021-05-27 16:52:51 +0800

    remove ExampleLib

--- Source/PythonVM/Private/PythonVM.cpp
+++ Source/PythonVM/Private/PythonVM.cpp
@@ -2,48 +2,17 @@
 
 #include "PythonVM.h"
 #include "Core.h"
-#include "Modules/ModuleManager.h"
-#include "Interfaces/IPluginManager.h"
-#include "PythonVMLibrary/ExampleLibrary.h"
+//#include "Modules/ModuleManager.h"
+//#include "Interfaces/IPluginManager.h"
 
 #define LOCTEXT_NAMESPACE "FPythonVMModule"
 
 void FPythonVMModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-
-	// Get the base directory of this plugin
-	FString BaseDir = IPluginManager::Get().FindPlugin("PythonVM")->GetBaseDir();
-
-	// Add on the relative location of the third party dll and load it
-	FString LibraryPath;
-#if PLATFORM_WINDOWS
-	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Binaries/ThirdParty/PythonVMLibrary/Win64/ExampleLibrary.dll"));
-#elif PLATFORM_MAC
-    LibraryPath = FPaths::Combine(*BaseDir, TEXT("Source/ThirdParty/PythonVMLibrary/Mac/Release/libExampleLibrary.dylib"));
-#endif // PLATFORM_WINDOWS
-
-	ExampleLibraryHandle = !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
-
-	if (ExampleLibraryHandle)
-	{
-		// Call the test function in the third party library that opens a message box
-		//ExampleLibraryFunction();
-	}
-	else
-	{
-		FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("ThirdPartyLibraryError", "Failed to load example third party library"));
-	}
 }
 
 void FPythonVMModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-
-	// Free the dll handle
-	FPlatformProcess::FreeDllHandle(ExampleLibraryHandle);
-	ExampleLibraryHandle = nullptr;
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/PythonVM/Public/PythonVM.h
+++ Source/PythonVM/Public/PythonVM.h
@@ -12,7 +12,4 @@ public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 
-private:
-	/** Handle to the test dll we will load */
-	void*	ExampleLibraryHandle;
 };
